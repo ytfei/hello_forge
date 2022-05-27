b1cadf667700d868e44dbab5f1a1d14b8c1fbdc0
yangtf <yangtf@corp.langzhihe.com>
2022-05-28 07:26:40 +0800

    add remappings

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin/token/ERC20/ERC20.sol";
 
-contract Contract is ERC20 {
+contract MyToken is ERC20 {
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
         _mint(msg.sender, 100000000);
     }
